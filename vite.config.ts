417ea69bc03c4a8ff5019ed7d5f78e392daf13a8
nguyenquocdatoriginals <quocdat87vt@gmail.com>
2024-05-29 03:23:10 +0700

    update system

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteImagemin from 'vite-plugin-imagemin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteImagemin()],
+  assetsInclude: ['**/*.JPG'],
+});
